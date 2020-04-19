f5b936c8677d873136036b103dff8ecffceff14f
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2020-04-19 18:56:16 -0400

    Initial commit

---
+++ app/src/main/java/com/lambton/androidcapstoneapp/HomeActivity.java
@@ -0,0 +1,48 @@
+package com.lambton.androidcapstoneapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class HomeActivity extends MainActivity {
+    TextView nameTv;
+    TextView emailTv;
+    Button logoutbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setTitle("ACTLABS SAMPLE PAGE");
+        context = this;
+        init();
+        setContentView(R.layout.activity_home);
+
+        //link views
+        getViews();
+    }
+
+    private void getViews() {
+        nameTv = findViewById(R.id.nameTv);
+        nameTv.setText(sharedPreferences.getString("name",""));
+        emailTv = findViewById(R.id.emailTv);
+        emailTv.setText(sharedPreferences.getString("email",""));
+        logoutbtn = findViewById(R.id.logoutBtn);
+
+        //make logout
+        logoutbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Redirect back to login page
+                logout();
+                intent = new Intent(context,LoginActivity.class);
+                //remove all previous stack activities
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+}
